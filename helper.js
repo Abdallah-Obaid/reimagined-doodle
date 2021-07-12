24a9c841d8798bcc106396612fbca9a833d3483e
Abdallah-Obaid <eng.abdallahobaid@gmail.com>
2021-07-12 10:31:18 +0300

    dynamic thresholds for live alerts

--- helper.js
+++ helper.js
@@ -1,5 +1,6 @@
 'use strict';
 const superagent = require('superagent');
+const CMS_URL = process.env.CMS_URL;
 var helper = {};
 helper.sendSmokeAlert= function(message){
   superagent.get(`https://test.penguinin.com/cms_ahmad/Alerts/SendALert?type=3&message=${message}`)
@@ -12,6 +13,22 @@ helper.sendSmokeAlert= function(message){
       console.log('Smoke alert error: ', err);
 
     });
+};
 
+/** 
+ * This function will get the thresholds for alerts from Cms DB
+ * @param {obj} req 
+ * @param {obj} res 
+ * @param {function} next 
+ */
+helper.getThresholds= async function() {
+  var thresholdsData = await superagent.get(`${CMS_URL}/Alerts/GetAlertThresholds`)
+    .then(thresholds => {
+      return thresholds.body;
+    })
+    .catch(err => {
+      console.log('Thresholds for alert error: ', err);
+    });
+  return thresholdsData;
 };
 module.exports = helper;
